08c19d23004586f701983fe75166cb4e5a789169
limch <hnwhlmc@163.com>
2024-04-03 14:04:27 +0800

    feat:自动化触发器 选择定时任务时不弹出aitable价格页面

--- packages/datasheet/src/pc/components/robot/robot_detail/trigger/robot_trigger_create.tsx
+++ packages/datasheet/src/pc/components/robot/robot_detail/trigger/robot_trigger_create.tsx
@@ -160,10 +160,10 @@ export const RobotTriggerCreateForm = ({ robotId, triggerTypes, preTriggerId }: 
   }
 
   const handleCreateFormChange = (triggerTypeId: string) => {
-    if (triggerTypeId === timeScheduleTriggerType?.triggerTypeId && !IS_ENTERPRISE) {
-      window.open('https://aitable.ai/pricing/');
-      return;
-    }
+    // if (triggerTypeId === timeScheduleTriggerType?.triggerTypeId && !IS_ENTERPRISE) {
+    //   window.open('https://aitable.ai/pricing/');
+    //   return;
+    // }
 
     if (triggerTypeId) {
       debouncedCreateTrigger(triggerTypeId);
